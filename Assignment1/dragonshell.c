cd8e4a62d703205456386b77ad5e19037a61ec16
xiacijie <cijie@ualberta.ca>
2019-09-19 22:56:55 -0600

    init

---
+++ Assignment1/dragonshell.c
@@ -0,0 +1,27 @@
+#include <stddef.h>
+#include <string.h>
+
+/**
+ * @brief Tokenize a C string 
+ * 
+ * @param str - The C string to tokenize 
+ * @param delim - The C string containing delimiter character(s) 
+ * @param argv - A char* array that will contain the tokenized strings
+ * Make sure that you allocate enough space for the array.
+ */
+void tokenize(char* str, const char* delim, char ** argv) {
+  char* token;
+  token = strtok(str, delim);
+  for(size_t i = 0; token != NULL; ++i){
+    argv[i] = token;
+  token = strtok(NULL, delim);
+  }
+}
+
+int main(int argc, char **argv) {
+  // print the string prompt without a newline, before beginning to read
+  // tokenize the input, run the command(s), and print the result
+  // do this in a loop
+
+  return 0;
+}
---
+++ Assignment1/dragonshell.cc
@@ -0,0 +1,35 @@
+#include <vector>
+#include <string>
+#include <cstring>
+
+/**
+ * @brief Tokenize a string 
+ * 
+ * @param str - The string to tokenize
+ * @param delim - The string containing delimiter character(s)
+ * @return std::vector<std::string> - The list of tokenized strings. Can be empty
+ */
+std::vector<std::string> tokenize(const std::string &str, const char *delim) {
+  char* cstr = new char[str.size() + 1];
+  std::strcpy(cstr, str.c_str());
+
+  char* tokenized_string = strtok(cstr, delim);
+
+  std::vector<std::string> tokens;
+  while (tokenized_string != NULL)
+  {
+    tokens.push_back(std::string(tokenized_string));
+    tokenized_string = strtok(NULL, delim);
+  }
+  delete[] cstr;
+
+  return tokens;
+}
+
+int main(int argc, char **argv) {
+  // print the string prompt without a newline, before beginning to read
+  // tokenize the input, run the command(s), and print the result
+  // do this in a loop
+
+  return 0;
+}
