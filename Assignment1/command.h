4eed1bae6f272e264d5cd6c94176f93ab1a44118
xiacijie <cijie@ualberta.ca>
2019-09-21 00:58:52 -0600

    start

---
+++ Assignment1/command.h
@@ -0,0 +1,4 @@
+enum Command {
+    pwd,
+    cd
+};
--- Assignment1/dragonshell.cc
+++ Assignment1/dragonshell.cc
@@ -1,24 +1,30 @@
 #include <vector>
+#include <iostream>
 #include <string>
 #include <cstring>
 
+#include <unistd.h>
+#include "command.h"
+
+#define BUFFER_SIZE 1024
+using namespace std;
 /**
  * @brief Tokenize a string 
  * 
  * @param str - The string to tokenize
  * @param delim - The string containing delimiter character(s)
- * @return std::vector<std::string> - The list of tokenized strings. Can be empty
+ * @return vector<string> - The list of tokenized strings. Can be empty
  */
-std::vector<std::string> tokenize(const std::string &str, const char *delim) {
+vector<string> tokenize(const string &str, const char *delim) {
   char* cstr = new char[str.size() + 1];
-  std::strcpy(cstr, str.c_str());
+  strcpy(cstr, str.c_str());
 
   char* tokenized_string = strtok(cstr, delim);
 
-  std::vector<std::string> tokens;
+  vector<string> tokens;
   while (tokenized_string != NULL)
   {
-    tokens.push_back(std::string(tokenized_string));
+    tokens.push_back(string(tokenized_string));
     tokenized_string = strtok(NULL, delim);
   }
   delete[] cstr;
@@ -30,6 +36,29 @@ int main(int argc, char **argv) {
   // print the string prompt without a newline, before beginning to read
   // tokenize the input, run the command(s), and print the result
   // do this in a loop
+  string test = "This is a   test string";
+  vector<string> v = tokenize(test," ");
+  for (size_t i = 0; i<v.size();i++){
+    cout << v[i] << endl;
+  }
+
+  while (true){
+    cout << "dragon shell > ";
+    string line;
+    
+    //get the input from termial
+    cin >> line;
+
+    //tokenize the commands
+    vector<string> commands =  tokenize(line, " ");
+    
+
+    char path[BUFFER_SIZE];
+    cout << getcwd(path, BUFFER_SIZE);
+
+
+    cout << line << endl;
+  }
 
   return 0;
 }
