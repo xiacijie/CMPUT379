3430f071dd8ff60470da2f7c8437c5f4f7adf2a1
Cijie Xia <cijie@ualberta.ca>
2019-11-23 22:56:18 -0800

    ass3 init

---
+++ Assignment3/FileSystem.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdint.h>
+
+typedef struct {
+	char name[5];        // Name of the file or directory
+	uint8_t used_size;   // Inode state and the size of the file or directory
+	uint8_t start_block; // Index of the start file block
+	uint8_t dir_parent;  // Inode mode and the index of the parent inode
+} Inode;
+
+typedef struct {
+	char free_block_list[16];
+	Inode inode[126];
+} Super_block;
+
+void fs_mount(char *new_disk_name);
+void fs_create(char name[5], int size);
+void fs_delete(char name[5]);
+void fs_read(char name[5], int block_num);
+void fs_write(char name[5], int block_num);
+void fs_buff(uint8_t buff[1024]);
+void fs_ls(void);
+void fs_resize(char name[5], int new_size);
+void fs_defrag(void);
+void fs_cd(char name[5]);
